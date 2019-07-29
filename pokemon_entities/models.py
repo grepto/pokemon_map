6983e92dd050e543cd4fef3ac63995429cc0e00b
grepto <grepto@gmail.com>
2019-07-29 10:44:02 +0300

    allow nulls for unnecessary fields in models Pokemon, Pokemon_entity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField('название', max_length=200)
-    title_en = models.CharField('английское название', max_length=200)
-    title_jp = models.CharField('японское название', max_length=200)
-    description = models.TextField('описание', max_length=2000)
+    title_en = models.CharField('английское название', null=True, blank=True, max_length=200)
+    title_jp = models.CharField('японское название', null=True, blank=True, max_length=200)
+    description = models.TextField('описание', null=True, blank=True, max_length=2000)
     image = models.ImageField(upload_to='pokemons', null=True, blank=True, verbose_name='изображение')
     next_evolution = models.OneToOneField('self', on_delete=models.CASCADE, null=True, blank=True,
                                           related_name='previous_evolution', verbose_name='в кого эволюционирует')
@@ -20,8 +20,8 @@ class PokemonEntity(models.Model):
     lon = models.FloatField('долгота')
     appeared_at = models.DateTimeField('появится')
     disappeared_at = models.DateTimeField('исчезнет')
-    level = models.IntegerField('уровень')
-    health = models.IntegerField('здоровье')
-    strength = models.IntegerField('прочность')
-    defence = models.IntegerField('защита')
-    stamina = models.IntegerField('выносливость')
+    level = models.IntegerField('уровень', null=True, blank=True)
+    health = models.IntegerField('здоровье', null=True, blank=True)
+    strength = models.IntegerField('прочность', null=True, blank=True)
+    defence = models.IntegerField('защита', null=True, blank=True)
+    stamina = models.IntegerField('выносливость', null=True, blank=True)
